7052d7754f4a15ff545473c9d897455629519c7e
d-cho623 <s3.ikeike344@gmail.com>
2021-05-17 15:43:11 +0900

    Revert "bootstrap導入"
    This reverts commit d52121f153e5ce9bab0fce00f8daed5663b36f9a.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap';
-import '../stylesheets/application';
-
 require("@rails/ujs").start()
 require("channels")
 require("../total_price")
 require('jquery')
-
-
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
