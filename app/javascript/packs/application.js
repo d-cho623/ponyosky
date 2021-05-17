4b2d40a2409393e6285f277bc60eeb840ffdc0a5
d-cho623 <s3.ikeike344@gmail.com>
2021-05-17 13:33:21 +0900

    jqueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("channels")
 require("../total_price")
+require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
