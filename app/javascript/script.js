c73fb4c3a94fefcc5eef1e5be82a292ddfb792b0
d-cho623 <s3.ikeike344@gmail.com>
2021-05-25 21:10:58 +0900

    不要な記述の削除

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,12 +1,11 @@
 window.addEventListener('load', () => {
-const targetElement = document.querySelectorAll(".animationTarget");
-document.addEventListener("scroll", function(){
-  for(let i = 0; i < targetElement.length; i++ ){
-    const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * .2
-    if(window.innerHeight > getElementDistance) {
-      targetElement[i].classList.add("show");
-    }
-  };
-});
-console.log(targetElement);
+  const targetElement = document.querySelectorAll(".animationTarget");
+  document.addEventListener("scroll", function(){
+    for(let i = 0; i < targetElement.length; i++ ){
+      const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * .2
+      if(window.innerHeight > getElementDistance) {
+        targetElement[i].classList.add("show");
+      }
+    };
+  });
 });
