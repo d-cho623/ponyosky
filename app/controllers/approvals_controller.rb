c9568a1e8643626d70baf7e8b9d03bdd9a797aa8
d-cho623 <s3.ikeike344@gmail.com>
2021-05-26 12:33:09 +0900

    申請が承認及び承認取り消し後の遷移先をroot_pathに変更

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,12 +1,12 @@
 class ApprovalsController < ApplicationController
   def create
     @approval = current_user.approvals.create(item_id: params[:item_id])
-    redirect_to item_path(@approval.item_id) 
+    redirect_to root_path
   end
   
   def destroy
     @approval = Approval.find_by(item_id: params[:item_id], user_id: current_user.id)
     @approval.destroy
-    redirect_to item_path(@approval.item_id)
+    redirect_to root_path
   end
 end
